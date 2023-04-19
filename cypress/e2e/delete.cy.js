e3dedc98ddbd5cfd6e1f40c390720d642f50649a
Tanya Medvedeva <t.me@onerep.com>
2023-04-19 07:46:51 +0200

    Init

---
+++ cypress/e2e/delete.cy.js
@@ -0,0 +1,26 @@
+import {loginKey} from '../fixtures/Login.json';
+import {urls} from '../fixtures/Login.json';
+import {onContactPage} from '../support/page_objects/contactPage';
+
+describe('Login', () => {
+
+  beforeEach('Login', () => {
+    cy.visit(urls.urlLogin).get(onContactPage.Email).type(loginKey.emailKey);
+    cy.get(onContactPage.Password).type(loginKey.passwordKey);
+    cy.get(onContactPage.Submit).click();
+  });
+
+  it('delete first', () => {
+    cy.get(onContactPage.ContactRow).eq(0).click();
+    cy.get(onContactPage.Delete).click();
+    cy.url().should('eq', urls.urlContactList);
+    cy.get(onContactPage.ContactRow).should('have.length', 9);
+  });
+
+  it('delete 6th', () => {
+    cy.get(onContactPage.ContactRow).eq(5).click()
+    cy.get('#delete').click();
+    cy.url().should('eq', urls.urlContactList);   
+    cy.get(onContactPage.ContactRow).should('have.length', 8);
+  });
+})
---
+++ cypress/support/page_objects/contactPage.js
@@ -0,0 +1,13 @@
+export class contactPage {
+
+//login page
+get Email() { return '#email'; }
+get Password() { return '#password'; }
+get Submit() { return '#submit'; }
+//contactPage
+get ContactRow() { return '.contactTableBodyRow'; }
+//cardPage
+get Delete() { return '#delete'; }
+}
+
+export const onContactPage = new contactPage();
